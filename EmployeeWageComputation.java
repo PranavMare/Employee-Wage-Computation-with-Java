1e5c714deeb18e59e87fca3083ae861361993e56
Pranav <pranavmare99@gmail.com>
2020-09-08 00:18:50 +0530

    Added till max hours or working days

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -7,15 +7,18 @@ public class EmployeeWageComputation {
 		int empHrs=0;
 		int wage=0;
 		int totalempHours=0;
+		int totalWorkDays=0;
 		
-		for(int i=0;i<30;i++) {
-		
-			if(totalempHours>=100)break;
+		for(int i=0;i<31;i++) {	
+			
+			if(totalWorkDays>20 || totalempHours>=100)break;
 			
 			int empCheck=(int)(Math.random()*10%3);
+			
 			switch(empCheck) {
 			case 1:
 				empHrs=8;
+				totalWorkDays++;
 				wage+=empHrs*empRatePerHour;
 				totalempHours+=empHrs;
 				break;
@@ -23,6 +26,7 @@ public class EmployeeWageComputation {
 				empHrs=4;
 				wage+=empHrs*empRatePerHour;
 				totalempHours+=empHrs;
+				totalWorkDays++;
 				break;
 			}
 		}
