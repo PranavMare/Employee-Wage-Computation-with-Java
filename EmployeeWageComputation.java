687e9e9dc3ba4b8ef2fd0920e42b8d088112a2de
Pranav <pranavmare99@gmail.com>
2020-09-07 23:45:19 +0530

    Added using Switch

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -3,30 +3,19 @@ public class EmployeeWageComputation {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Welcome to Wage Computation");
-		
-		int empCheck=(int)(Math.random()*10%2);
-		if(1==empCheck)
-			System.out.println("Employee is Present");
-		else
-			System.out.println("Employee is Absent");	
-		
-		int wageperHour=20;
-		int fullTimeHour=8;
-		int fulltimewage=0;
-		if(empCheck==1) {
-			System.out.println(fulltimewage=wageperHour*fullTimeHour);
-			
-		}				
-		
-		int partTimeHour=4;
-		int partimeCheck=(int)(Math.random()*10%2);
-		int parttimeWage=0;
-		if(partimeCheck==1) {
-			System.out.println(parttimeWage=partTimeHour*wageperHour);
+		int empRatePerHour=20;
+		int empCheck=(int)(Math.random()*10%3);
+		int empHrs=0;
+		switch(empCheck) {
+		case 1:
+			empHrs=8;
+			break;
+		case 2:
+			empHrs=4;
+			break;
 		}
 		
-		
+		System.out.println(empHrs*empRatePerHour);	
 	}
 	
 }
