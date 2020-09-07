4224dd5a1a0659c37bf97fa2f5903c95a9ca81ea
Pranav <pranavmare99@gmail.com>
2020-09-07 23:52:50 +0530

    Added Monthly Wage

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -4,18 +4,24 @@ public class EmployeeWageComputation {
 	public static void main(String[] args) {
 		
 		int empRatePerHour=20;
-		int empCheck=(int)(Math.random()*10%3);
 		int empHrs=0;
-		switch(empCheck) {
-		case 1:
-			empHrs=8;
-			break;
-		case 2:
-			empHrs=4;
-			break;
+		int wage=0;
+		
+		for(int i=0;i<30;i++) {
+			int empCheck=(int)(Math.random()*10%3);
+			switch(empCheck) {
+			case 1:
+				empHrs=8;
+				wage+=empHrs*empRatePerHour;
+				break;
+			case 2:
+				empHrs=4;
+				wage+=empHrs*empRatePerHour;
+				break;
+			}
 		}
 		
-		System.out.println(empHrs*empRatePerHour);	
+		System.out.println(wage);	
 	}
 	
 }
