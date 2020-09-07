a5db2fc479cac16286794103da4e4f86f0e1fd78
Pranav <pranavmare99@gmail.com>
2020-09-07 23:11:21 +0530

    Added Wage

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -4,13 +4,19 @@ public class EmployeeWageComputation {
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Wage Computation");
-	
+		
 		int isPresent=1;
 		int empCheck=(int)(Math.random()*10%2);
 		if(isPresent==empCheck)
 			System.out.println("Employee is Present");
 		else
-			System.out.println("Employee us Absent");			
+			System.out.println("Employee is Absent");	
+		
+		int wageperHour=20;
+		int fullDayHour=8;
+		int wage=0;
+		if(empCheck==1) {
+			wage=wageperHour*fullDayHour;
+		}				
 	}
-
 }
