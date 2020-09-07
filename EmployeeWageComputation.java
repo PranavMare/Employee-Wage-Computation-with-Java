28ac7a44d45d5e3ed3b4d53d4fc5c65deeac945b
Pranav <pranavmare99@gmail.com>
2020-09-08 00:06:21 +0530

    Added till Max Hours

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -6,17 +6,23 @@ public class EmployeeWageComputation {
 		int empRatePerHour=20;
 		int empHrs=0;
 		int wage=0;
+		int totalempHours=0;
 		
 		for(int i=0;i<30;i++) {
+		
+			if(totalempHours>=100)break;
+			
 			int empCheck=(int)(Math.random()*10%3);
 			switch(empCheck) {
 			case 1:
 				empHrs=8;
 				wage+=empHrs*empRatePerHour;
+				totalempHours+=empHrs;
 				break;
 			case 2:
 				empHrs=4;
 				wage+=empHrs*empRatePerHour;
+				totalempHours+=empHrs;
 				break;
 			}
 		}
